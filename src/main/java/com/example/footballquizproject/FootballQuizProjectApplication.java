f1e530a68b706f7a3e78329571c4c9242203d959
[sj.youns1027@gmail.com] <sj.youns1027@gmail.com>
2024-04-09 15:28:06 +0900

    GITHUB Connection TEST

---
+++ src/main/java/com/example/footballquizproject/FootballQuizProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballquizproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballQuizProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballQuizProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/footballquizproject/FootballQuizProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballquizproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballQuizProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
