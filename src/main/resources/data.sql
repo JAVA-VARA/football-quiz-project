cb2dc7bd57673d346687a85d24a25790973c138a
[sj.youns1027@gmail.com] <sj.youns1027@gmail.com>
2024-04-10 17:41:43 +0900

    [설정] - application.yml: spring, db, logging 설정. - data.sql: test date 삽입.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,45 @@
+insert into team_category (team_name, team_emblem, league, league_emblem) values
+('아틀레티코 마드리드', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('레알 마드리드', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('바르셀로나', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('레알 소시에다드', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('그라나다', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('첼시', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , '프리미어리그', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('나폴리', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , '세리에 A', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('도르트문트', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , '분데스리가 A', 'http://dummyimage.com/238x100.png/ff4444/ffffff');
+
+INSERT INTO players(name, image_url, team_name) values
+('a', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('b', 'http://dummyimage.com/238x100.png/ff4444/ffffff', '아틀레티코 마드리드'),
+('c', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('d', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('e', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('f', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('g', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('h', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('i', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('j', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('k', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('l', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('m', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('o', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('n', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('p', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('q', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('r', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('s', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('t', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('v', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
+('u', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드');
+
+INSERT INTO game_category(category_id,category_name, category_description, category_thumbnail ) values
+(1, '축구 선수 사진 보고 이름 맞추기', '축구 선수 사진을 보고 이름을 맞춰봐' , 'http://dummyimage.com/238x100.png/ff4444/ffffff');
+
+INSERT INTO alias_category(alias, corrected_answer) values
+('뉴비' , 0),
+('패션' , 5),
+('애매' , 10),
+('썩은물' , 15),
+('폐인' , 20);
+
+
