1aa2b712faf3d3af4369ca93339752e9e1203f56
[sj.youns1027@gmail.com] <sj.youns1027@gmail.com>
2024-04-12 15:00:35 +0900

    entity 변경에 따른 test 삽입 데이터 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,19 @@
-insert into team_category (team_name, team_emblem, league, league_emblem) values
-('아틀레티코 마드리드', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
-('레알 마드리드', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
-('바르셀로나', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
-('레알 소시에다드', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
-('그라나다', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , 'Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
-('첼시', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , '프리미어리그', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
-('나폴리', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , '세리에 A', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
-('도르트문트', 'http://dummyimage.com/238x100.png/ff4444/ffffff' , '분데스리가 A', 'http://dummyimage.com/238x100.png/ff4444/ffffff');
+insert into team_category (team_name, team_emblem, league) values
+('아틀레티코 마드리드', 'http://dummyimage.com/238x100.png/ff4444/ffffff','Laliga'),
+('레알 마드리드', 'http://dummyimage.com/238x100.png/ff4444/ffffff' ,'Laliga'),
+('바르셀로나', 'http://dummyimage.com/238x100.png/ff4444/ffffff' ,'Laliga'),
+('레알 소시에다드', 'http://dummyimage.com/238x100.png/ff4444/ffffff' ,'Laliga'),
+('그라나다', 'http://dummyimage.com/238x100.png/ff4444/ffffff' ,'Laliga'),
+('첼시', 'http://dummyimage.com/238x100.png/ff4444/ffffff','프리미어리그'),
+('나폴리', 'http://dummyimage.com/238x100.png/ff4444/ffffff', '세리에'),
+('도르트문트', 'http://dummyimage.com/238x100.png/ff4444/ffffff', '분데스리가');
+
+insert into league_category(league, league_emblem) values
+('Laliga', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('프리미어리그', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('세리에', 'http://dummyimage.com/238x100.png/ff4444/ffffff'),
+('분데스리가', 'http://dummyimage.com/238x100.png/ff4444/ffffff');
+
 
 INSERT INTO players(name, image_url, team_name) values
 ('a', 'http://dummyimage.com/238x100.png/ff4444/ffffff','아틀레티코 마드리드'),
@@ -32,8 +39,8 @@ INSERT INTO players(name, image_url, team_name) values
 ('v', 'http://dummyimage.com/102x100.png/dddddd/000000','아틀레티코 마드리드'),
 ('u', 'http://dummyimage.com/105x100.png/dddddd/000000','아틀레티코 마드리드');
 
-INSERT INTO game_category(category_id,category_name, category_description, category_thumbnail ) values
-(1, '축구 선수 사진 보고 이름 맞추기', '축구 선수 사진을 보고 이름을 맞춰봐' , 'http://dummyimage.com/238x100.png/ff4444/ffffff');
+INSERT INTO game_category(category_id,category_name, category_description, category_thumbnail, category_url ) values
+(1, '축구 선수 사진 보고 이름 맞추기', '축구 선수 사진을 보고 이름을 맞춰봐' , 'http://dummyimage.com/238x100.png/ff4444/ffffff', '/who-are-you');
 
 INSERT INTO alias_category(alias, min_correct_answers, max_correct_answers) values
 ('뉴비' , 0 , 5),
