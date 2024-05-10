81f92004c1aa3684e9c6b0a1412b5beabaab06a0
[sj.youns1027@gmail.com] <sj.youns1027@gmail.com>
2024-05-11 00:08:14 +0900

    @Scheduled 어노테이션 추가하여 지정된 시간에 크롤링되도록 설정 => 시즌, 이적시장 끝난 후 일주일 후 크롤링 시행 7일을 설정한 이유는 관련 페이지들이 업데이트 될 때 까지 시간을 둠.

--- src/main/java/com/example/footballquizproject/FootballQuizProjectApplication.java
+++ src/main/java/com/example/footballquizproject/FootballQuizProjectApplication.java
@@ -3,9 +3,11 @@ package com.example.footballquizproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication
 public class FootballQuizProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/footballquizproject/controller/WebCrawlingController.java
+++ src/main/java/com/example/footballquizproject/controller/WebCrawlingController.java
@@ -4,6 +4,7 @@ import com.example.footballquizproject.enumPack.*;
 import com.example.footballquizproject.service.CollectPlayersDataService;
 import com.example.footballquizproject.service.CollectTeamData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +17,7 @@ public class WebCrawlingController {
     private final CollectPlayersDataService collectPlayersData;
     private final CollectTeamData collectTeamData;
 
-    @GetMapping("/team")
-    public void crawlingTeamInfo() throws InterruptedException {
-        for (LeagueClubsURL L : LeagueClubsURL.values()) {
-            collectTeamData.collectTeamInfo(L);
-        }
-    }
-
-
+    @Scheduled(cron = "0 0 0 07 09 ?" , zone = "Asia/Seoul")
     @GetMapping("/players")
     public void crawlingPlayersInfo() throws Exception {
         collectPlayersByLeague(EPLClubsSquadURL.values());
@@ -31,6 +25,14 @@ public class WebCrawlingController {
         collectPlayersByLeague(LaligaClubsSquadURL.values());
         collectPlayersByLeague(SerieAClubsSquadURL.values());
         collectPlayersByLeague(League1ClubsSquadURL.values());
+    }
+
+    @Scheduled(cron = "0 0 0 07 09 ?" , zone = "Asia/Seoul")
+    @GetMapping("/team")
+    public void crawlingTeamInfo() throws InterruptedException {
+        for (LeagueClubsURL L : LeagueClubsURL.values()) {
+            collectTeamData.collectTeamInfo(L);
+        }
     }
 
     private void collectPlayersByLeague(ClubsSquadURLProvider[] squadURLs) throws Exception {
