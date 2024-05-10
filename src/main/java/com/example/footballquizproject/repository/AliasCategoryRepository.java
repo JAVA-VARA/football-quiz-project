988d0ff76e1a4f4143f6fe638b84300ae24a522b
[sj.youns1027@gmail.com] <sj.youns1027@gmail.com>
2024-05-11 00:06:19 +0900

    1. Entity AliasCategory.java -> LevelCategory 변경
    2. 불필요한 주석 제거

--- src/main/java/com/example/footballquizproject/repository/AliasCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.footballquizproject.repository;
-
-import com.example.footballquizproject.domain.AliasCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AliasCategoryRepository extends JpaRepository<AliasCategory, String> {
-    AliasCategory findByMinCorrectAnswersLessThanEqualAndMaxCorrectAnswersGreaterThanEqual(int correctAnswers, int correctAnswers1);
-}
