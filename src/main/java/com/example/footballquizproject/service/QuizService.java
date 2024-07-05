50b8d70c6f89d50d631b907b5fe17ebaa7288abc
[sj.youns1027@gmail.com] <sj.youns1027@gmail.com>
2024-07-05 11:00:25 +0900

    QuizService에 있었던 메서드들 기능에 따라 별도의 클래스로 분리함.

--- src/main/java/com/example/footballquizproject/service/QuizService.java
+++ src/main/java/com/example/footballquizproject/service/QuizService.java
@@ -1,28 +1,24 @@
 package com.example.footballquizproject.service;
 
 import com.example.footballquizproject.config.PlayerSetCache;
-import com.example.footballquizproject.domain.PlayerInQuizSet;
 import com.example.footballquizproject.domain.Players;
-import com.example.footballquizproject.domain.QuizSet;
-import com.example.footballquizproject.domain.TeamCategory;
 import com.example.footballquizproject.dto.QuizDto;
-import com.example.footballquizproject.repository.PlayerInQuizSetRepository;
 import com.example.footballquizproject.repository.PlayersRepository;
-import com.example.footballquizproject.repository.QuizSetRepository;
-import com.example.footballquizproject.repository.TeamCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class QuizService {
+
     private final PlayersRepository playersRepository;
-    private final QuizSetRepository quizSetRepository;
-    private final PlayerInQuizSetRepository playerInQuizSetRepository;
-    private final TeamCategoryRepository teamCategoryRepository;
+    private final QuizSetService quizSetService;
     private final PlayerSetCache cache;
 
     public List<QuizDto> createQuizSet(Long teamId, int numOfQuestions) {
@@ -32,34 +28,15 @@ public class QuizService {
         if(playersSetByTeamId == null){
             playersSetByTeamId = savePlayersListByTeamIdToCache(teamId);
         }
+
         // 문제 수에 따라 선수 랜덤 선택
         List<Players> selectedPlayers = selectRandomPlayers(playersSetByTeamId, numOfQuestions);
-
-        Long quizId = saveQuizSet(teamId, selectedPlayers);
+        Long quizId = quizSetService.saveQuizSet(teamId, selectedPlayers);
 
         // 선택한 선수의 정보를 DTO로 변환
         return convertToQuizDtos(quizId, selectedPlayers);
-
     }
 
-    // 선택한 선수를 퀴즈 세트에 삽입
-    public Long saveQuizSet(Long teamId, List<Players> selectedPlayers) {
-        TeamCategory team = teamCategoryRepository.findByTeamId(teamId);
-        QuizSet quizSet = new QuizSet();
-        quizSet.setTeamCategory(team);
-        quizSetRepository.save(quizSet);
-
-        List<PlayerInQuizSet> quizSetPlayers = new ArrayList<>();
-        for (int i = 0; i < selectedPlayers.size(); i++) {
-            PlayerInQuizSet quizSetPlayer = createPlayerInQuizSet(quizSet, selectedPlayers.get(i), i + 1);
-            quizSetPlayers.add(quizSetPlayer);
-            quizSet.addPlayerInQuizSet(quizSetPlayer);
-        }
-
-        playerInQuizSetRepository.saveAll(quizSetPlayers);
-
-        return quizSet.getQuizId();
-    }
 
     private Set<Players> getPlayersListFromCache(Long teamId) {
         String cacheKey = "playersList_" + teamId;
@@ -77,13 +54,6 @@ public class QuizService {
         return players;
     }
 
-    private List<QuizDto> convertToQuizDtos(Long quizId, List<Players> selectedPlayers) {
-        return selectedPlayers.stream()
-                .map(players ->
-                        new QuizDto(quizId, players.getImageUrl(), players.getFullName(), players.getFirstName(), players.getMiddleName(), players.getLastName()))
-                .collect(Collectors.toList());
-    }
-
     private List<Players> selectRandomPlayers(Set<Players> playersSetByTeamId, int numOfQuestions) {
         List<Players> playersListByTeamId = new ArrayList<>(playersSetByTeamId);
         Collections.shuffle(playersListByTeamId);
@@ -92,11 +62,10 @@ public class QuizService {
                 .toList();
     }
 
-    private PlayerInQuizSet createPlayerInQuizSet(QuizSet quizSet, Players player, int orderIndex) {
-        PlayerInQuizSet quizSetPlayer = new PlayerInQuizSet();
-        quizSetPlayer.setQuizSet(quizSet);
-        quizSetPlayer.setPlayer(player);
-        quizSetPlayer.setOrderIndex(orderIndex);
-        return quizSetPlayer;
+    private List<QuizDto> convertToQuizDtos(Long quizId, List<Players> selectedPlayers) {
+        return selectedPlayers.stream()
+                .map(players ->
+                        new QuizDto(quizId, players.getImageUrl(), players.getFullName(), players.getFirstName(), players.getMiddleName(), players.getLastName()))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/example/footballquizproject/service/QuizServiceTest.java
+++ src/test/java/com/example/footballquizproject/service/QuizServiceTest.java
@@ -1,44 +1,97 @@
 package com.example.footballquizproject.service;
 
+import com.example.footballquizproject.config.PlayerSetCache;
+import com.example.footballquizproject.domain.LeagueCategory;
 import com.example.footballquizproject.domain.Players;
+import com.example.footballquizproject.domain.TeamCategory;
+import com.example.footballquizproject.dto.QuizDto;
 import com.example.footballquizproject.repository.PlayersRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
+import java.util.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@DisplayName("QuizServiceTest")
+@ExtendWith(MockitoExtension.class)
 class QuizServiceTest {
-    @Autowired
-    MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
+    @Mock
+    private PlayersRepository playersRepository;
 
-    @Autowired
-    PlayersRepository playersRepository;
+    @Mock
+    private PlayerSetCache cache;
+
+
+    @InjectMocks
+    private QuizService quizService;
+
+    private Long teamId;
+
+    private int numberOfQuestions;
+    private Set<Players> playersSet;
+    private List<Players> playersList;
+    private TeamCategory team;
+
+    @BeforeEach
+    void setUp() {
+        LeagueCategory leagueCategory = new LeagueCategory("leagueName", "leagueEmblem");
+        teamId = 1L;
+        team = new TeamCategory("teamName", "teamDescription", leagueCategory);
+        numberOfQuestions = 10;
+
+        Players player1 = new Players("url1", "FullName1", "FirstName1", "MiddleName1", "LastName1", "23/24", team, "1");
+        Players player2 = new Players("url2", "FullName2", "FirstName2", "MiddleName2", "LastName2", "23/24", team, "2");
+        Players player3 = new Players("url3", "FullName3", "FirstName3", "MiddleName3", "LastName3", "23/24", team, "3");
+        Players player4 = new Players("url4", "FullName4", "FirstName4", "MiddleName4", "LastName4", "23/24", team, "4");
+        Players player5 = new Players("url5", "FullName5", "FirstName5", "MiddleName5", "LastName5", "23/24", team, "5");
+        Players player6 = new Players("url6", "FullName6", "FirstName6", "MiddleName6", "LastName6", "23/24", team, "6");
+        Players player7 = new Players("url7", "FullName7", "FirstName7", "MiddleName7", "LastName7", "23/24", team, "7");
+        Players player8 = new Players("url8", "FullName8", "FirstName8", "MiddleName8", "LastName8", "23/24", team, "8");
+        Players player9 = new Players("url9", "FullName9", "FirstName9", "MiddleName9", "LastName9", "23/24", team, "9");
+        Players player10 = new Players("url10", "FullName10", "FirstName10", "MiddleName10", "LastName10", "23/24", team, "10");
+
+        playersSet = new HashSet<>(Arrays.asList(player1, player2, player3, player4, player5, player6, player7, player8, player9, player10));
+        playersList = new ArrayList<>(playersSet);
+
+    }
 
     @Test
-    @DisplayName("팀을 기준 랜덤으로 n명의 선수 사진 URL 과 이름을 가져온다.")
-    void testPick10PlayersByTeamId(){
-        //GIVEN
-        Long teamId = 1L;
+    void createQuizSet_withPlayersInCache() {
+        //given
+        when(cache.containsKey("playersList_" + teamId)).thenReturn(true);
+        when(cache.get("playersList_" + teamId)).thenReturn(playersSet);
 
-        //WHEN
-        List<Players> players = playersRepository.findRandomPlayersByTeamIdPick10(teamId);
+        //when
+        List<QuizDto> quizDto = quizService.createQuizSet(teamId, numberOfQuestions);
 
-        //THEN
-        assertEquals(players.size(), 10);
-        assertThat(players.get(0).getTeam().getTeamId()).isEqualTo(teamId);
+        //then
+        assertNotNull(quizDto);
+        assertEquals(numberOfQuestions, quizDto.size());
+        verify(cache, times(1)).containsKey("playersList_"+teamId);
+        verify(cache, times(1)).get("playersList_" + teamId);
+    }
+
+    @Test
+    void createQuizSet_withPlayersNotInCache() {
+        //given
+        when(cache.containsKey("playersList_" + teamId)).thenReturn(false);
+        when(playersRepository.findByTeamId(teamId)).thenReturn(playersSet);
+
+        //when
+        List<QuizDto> quizDto = quizService.createQuizSet(teamId, numberOfQuestions);
+
+        //then
+        assertNotNull(quizDto);
+        assertEquals(numberOfQuestions, quizDto.size());
+        verify(playersRepository, times(1)).findByTeamId(teamId);
+        verify(cache, times(1)).containsKey("playersList_" + teamId);
+        verify(cache, times(1)).put("playersList_" + teamId, playersSet);
     }
 }
